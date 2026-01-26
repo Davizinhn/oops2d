983b3ec83e27e91d7a316be42bfcd3d92cabd061
dav <73536483+Davizinhn@users.noreply.github.com>
2026-01-26 10:05:21 -0300

    feat: text rendering and basic formatting

---
+++ Rendering/Text/Text2D.cs
@@ -0,0 +1,30 @@
+﻿using oops2d.Core;
+using Raylib_cs;
+using System.Numerics;
+
+namespace oops2d.Rendering.Text
+{
+    public class Text2D : Object2D
+    {
+        public TextFormat format;
+
+        public Text2D(string text, Vector2 position, int size = 12, Color color = new Color(), string fontPath = "")
+        {
+            format = new TextFormat(text, size, color);
+            format.SetFont(fontPath);
+            transform.Position = position;
+        }
+
+        public Text2D(string text, Vector2 position, int size = 12, Color color = new Color(), Font font = default)
+        {
+            format = new TextFormat(text, size, color, font);
+            transform.Position = position;
+        }
+
+        public override void Draw(Scene2D scene)
+        {
+            Raylib.DrawTextPro(format.Font, format.Text, transform.Position, new Vector2(0, 0), 0, format.Size, 1, format.TextColor);
+            base.Draw(scene);
+        }
+    }
+}
---
+++ Rendering/Text/TextFormat.cs
@@ -0,0 +1,37 @@
+﻿using oops2d.Core;
+using Raylib_cs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oops2d.Rendering.Text
+{
+    public class TextFormat
+    {
+        public string Text = "";
+        public int Size;
+        public Color TextColor = new Color();
+        public Font Font = Raylib.GetFontDefault();
+
+        public TextFormat(string text, int size = 12, Color color = new Color(), Font font = default)
+        {
+            Text = text;
+            Size = size;
+            TextColor = color;
+            Font = font;
+        }
+
+        public void SetFont(string fontPath)
+        {
+            if (string.IsNullOrEmpty(fontPath))
+            {
+                return;
+            }
+
+            Font = Raylib.LoadFont(fontPath);
+        }
+    }
+}
